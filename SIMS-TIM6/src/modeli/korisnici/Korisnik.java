107be5949c9cdba61da56538fe291c00928be9e6
Nemanja Jankovic <nemanja997@gmail.com>
2018-07-10 02:02:01 +0200

    Testiranje GUI-ja.
    Dodati default korisnici, login sistem i smena prozora.

--- SIMS-TIM6/src/modeli/korisnici/Korisnik.java
+++ SIMS-TIM6/src/modeli/korisnici/Korisnik.java
@@ -27,4 +27,6 @@ public abstract class Korisnik {
 		this.password = password;
 	}
 	
+	
+	
 }
---
+++ SIMS-TIM6/src/prozori/ZaposleniUCentraliProzor.java
@@ -0,0 +1,18 @@
+package prozori;
+
+import javax.swing.JFrame;
+
+public class ZaposleniUCentraliProzor extends JFrame {
+	
+	public ZaposleniUCentraliProzor() {
+		initUI();
+	}
+	
+	private void initUI() {
+		setTitle("ZaposleniUCentraliProzor example");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
